5aa568a2f7bf5763ba597cd29b9c0a5b7a052b62
Nash Kissel <nashkissel@gmail.com>
2018-10-31 14:00:08 -0400

    Add project files.

---
+++ ArrayEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] score = { 1, 5, 6, 2, 8, 15, 13, 17, 4, 12 };
+                int sum = 0;
+                for (int i = 0; i <= 9; i++)
+                {
+                    if (score[i] % 2 == 0)
+                    {
+                        sum = sum + score[i];
+                    }
+                }
+                Console.WriteLine("Sum:" + sum);
+            }
+        }
+    }
+
+}
